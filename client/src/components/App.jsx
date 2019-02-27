a1530f12f2ad26cd24b9a8b133747da3f93b701c
Santiago Blas Beitia <santiagob.beitia@gmail.com>
2019-02-27 15:21:37 -0300

    http requests al server con axios

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,5 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
+import api from '../services/api';
 
-const App = () => <div>App works</div>;
+// const App = () => <div>App works</div>;
+
+class App extends Component {
+    async componentDidMount() {
+        const result = await api.call('post', 'auth/login', {
+            username: 'santiago',
+            password: 'password'
+        });
+
+        console.log(result);
+    }
+
+    render() {
+        return <div>App Works</div>
+    }
+}
 
 export default App;
