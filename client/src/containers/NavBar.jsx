d3c033275dd0ba07c7b150424cba99a7f99244f6
santiagoblas <santiagob.beitia@gmail.com>
2019-03-18 11:29:44 -0300

    App Navigation

--- client/src/containers/NavBar.jsx
+++ client/src/containers/NavBar.jsx
@@ -1,5 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 
-const NavBar = () => (<div>NavBar Works</div>);
+import { logout } from '../store/actions';
+ 
+const NavBar = ({ auth, logout }) => (<div>
+    <ul>
+        <li><Link to='/register'>Register</Link></li>
+        <li><Link to='/login'>Login</Link></li>
+        <li><a onClick={logout}>Logout</a></li>
+    </ul>
+    {auth.isAuthenticated && (<p>Logged in as {auth.user.username}</p>)}
+</div>);
 
-export default NavBar;
+export default connect(store => ({auth: store.auth}), {logout})(NavBar); 
