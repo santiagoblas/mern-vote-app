d5204ebdefe4dff8de3eac599ac1c6a2e568c275
santiagoblas <santiagob.beitia@gmail.com>
2019-03-18 12:56:17 -0300

    Agregando encuestas al store de Redux

--- client/src/store/reducers/index.js
+++ client/src/store/reducers/index.js
@@ -1,9 +1,12 @@
-import {combineReducers} from 'redux';
+import { combineReducers } from 'redux';
 
 import error from './error';
 import auth from './auth';
+import { polls, currentPoll } from './polls';
 
 export default combineReducers({
     auth,
-    error
+    error,
+    polls,
+    currentPoll
 });
